78064d8efca402bd893173cb2b6a9a5ceff61508
DonaldLu <lugenius2001@gmail.com>
2025-10-20 16:21:58 +0800

    新增柱、牆, 依樓層分層建立之功能

--- TYBIM/LayersForm.Designer.cs
+++ TYBIM/LayersForm.Designer.cs
@@ -37,15 +37,22 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.t_level_comboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.b_level_comboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.byLevelCB = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // cancelBtn
             // 
             this.cancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelBtn.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.cancelBtn.Location = new System.Drawing.Point(291, 316);
+            this.cancelBtn.Location = new System.Drawing.Point(449, 316);
             this.cancelBtn.Margin = new System.Windows.Forms.Padding(4);
             this.cancelBtn.Name = "cancelBtn";
             this.cancelBtn.Size = new System.Drawing.Size(80, 35);
@@ -58,7 +65,7 @@
             // 
             this.sureBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.sureBtn.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.sureBtn.Location = new System.Drawing.Point(203, 316);
+            this.sureBtn.Location = new System.Drawing.Point(361, 316);
             this.sureBtn.Margin = new System.Windows.Forms.Padding(4);
             this.sureBtn.Name = "sureBtn";
             this.sureBtn.Size = new System.Drawing.Size(80, 35);
@@ -118,9 +125,8 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.groupBox1.Controls.Add(this.allRbtn);
             this.groupBox1.Controls.Add(this.listView1);
             this.groupBox1.Controls.Add(this.allCancelRbtn);
@@ -133,9 +139,8 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.groupBox2.Controls.Add(this.radioBtnPanel);
             this.groupBox2.Location = new System.Drawing.Point(230, 12);
             this.groupBox2.Name = "groupBox2";
@@ -144,11 +149,84 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "請選擇要翻模的類型";
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox3.Controls.Add(this.byLevelCB);
+            this.groupBox3.Controls.Add(this.t_level_comboBox);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.b_level_comboBox);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(375, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(153, 295);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            // 
+            // t_level_comboBox
+            // 
+            this.t_level_comboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.t_level_comboBox.FormattingEnabled = true;
+            this.t_level_comboBox.Location = new System.Drawing.Point(7, 165);
+            this.t_level_comboBox.Name = "t_level_comboBox";
+            this.t_level_comboBox.Size = new System.Drawing.Size(140, 25);
+            this.t_level_comboBox.TabIndex = 10;
+            this.t_level_comboBox.Text = "請選擇頂部樓層";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(28, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 26);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "頂部樓層";
+            // 
+            // b_level_comboBox
+            // 
+            this.b_level_comboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.b_level_comboBox.FormattingEnabled = true;
+            this.b_level_comboBox.Location = new System.Drawing.Point(7, 91);
+            this.b_level_comboBox.Name = "b_level_comboBox";
+            this.b_level_comboBox.Size = new System.Drawing.Size(140, 25);
+            this.b_level_comboBox.TabIndex = 8;
+            this.b_level_comboBox.Text = "請選擇基準樓層";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(28, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "基準樓層";
+            // 
+            // byLevelCB
+            // 
+            this.byLevelCB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.byLevelCB.AutoSize = true;
+            this.byLevelCB.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.byLevelCB.Location = new System.Drawing.Point(32, 206);
+            this.byLevelCB.Name = "byLevelCB";
+            this.byLevelCB.Size = new System.Drawing.Size(92, 21);
+            this.byLevelCB.TabIndex = 11;
+            this.byLevelCB.Text = "分樓層建立";
+            this.byLevelCB.UseVisualStyleBackColor = true;
+            // 
             // LayersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.ClientSize = new System.Drawing.Size(542, 361);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.sureBtn);
@@ -162,6 +240,8 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -176,5 +256,11 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox t_level_comboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox b_level_comboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox byLevelCB;
     }
 }
